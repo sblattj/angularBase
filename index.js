a24f11ad66905d4a16cd2c007aeb3229fa0a09d3
Stephen Blatt <sblattj@gmail.com>
2018-07-01 17:20:04 -0700

    Added Validation for Username/Email/Password

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-const mongoose = require('mongoose');
-const config = require('./config/database');
-const path = require('path');
-const authentication = require('./routes/authentication')(router);
+/* ===================
+   Import Node Modules
+=================== */
+const express = require('express'); // Fast, unopinionated, minimalist web framework for node.
+const app = express(); // Initiate Express Application
+const router = express.Router(); // Creates a new router object.
+const mongoose = require('mongoose'); // Node Tool for MongoDB
+const config = require('./config/database'); // Mongoose Config
+const path = require('path'); // NodeJS Package for file paths
+const authentication = require('./routes/authentication')(router); // Import Authentication Routes
+const bodyParser = require('body-parser'); // Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
+// Database Connection
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
     if (err) {
-        console.log('cnnt db fail', err);
+        console.log('Could NOT connect to database: ', err);
     } else {
-        console.log('cnnt to db:', config.db)
+        console.log('Connected to database: ' + config.db);
     }
 });
 
-app.use(express.static(__dirname + '/client/dist/client'));
+// Middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
 app.use('/authentication', authentication);
 
+// Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
+// Start Server: Listen on port 8080
 app.listen(8080, () => {
-    console.log('listening on port 8080');
+    console.log('Listening on port 8080');
 });
--- models/user.js
+++ models/user.js
@@ -1,9 +1,114 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+let emailLengthChecker = (email) => {
+    if (!email) {
+        return false;
+    } else {
+        if (email.length < 5 || email.length > 30) {
+            return false;
+        }
+        return true;
+    }
+}
+
+// Validate Function to check if valid e-mail format
+let validEmailChecker = (email) => {
+    // Check if e-mail exists
+    if (!email) {
+        return false; // Return error
+    } else {
+        // Regular expression to test for a valid e-mail
+        const regExp = 
+        new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return regExp.test(email); // Return regular expression test results (true or false)
+    }
+};
+
+const emailValidators = [
+    {
+        validator: emailLengthChecker, message: 'E-mail must be at least 5 and less than 31 characters'
+    },
+    {
+        validator: validEmailChecker,
+        nessage: 'Must be a valid E-mail'
+    }
+];
+
+let usernameLengthChecker = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        if (username.length < 3 || username.length > 14) {
+            return false;
+        }
+        return true;
+    }
+};
+
+let validUsername = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(username);
+    }
+};
+
+const usernameValidators = [{
+        validator: usernameLengthChecker,
+        message: "Username must be greater than 2 and less than 15 characters"
+    },
+    {
+        validator: validUsername,
+        message: "Username must be valid"
+    }
+];
+
+let passwordLengthChecker = (password) => {
+    if (!password) {
+        return false;
+    } else {
+        if (password.length < 4 || password.length > 35) {
+            return false;
+        }
+        return true;
+    }
+};
+
+//feel free to add validators for extra secure passwords as you please for production environments
+
+const passwordValidators = [
+    {
+        validator: passwordLengthChecker,
+        message: "Password must be between 4 and 35 characters"
+    }
+];
 
 const userSchema = new Schema({
-    email: {type: String, required: true, unique: true, lowercase: true },
-    username: {type: String, required: true, unique: true, lowercase: true },
-    password: {type: String, required: true }
+    email: {type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
+    username: {type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
+    password: {type: String, required: true, validate: passwordValidators }
 });
+
+
+
+userSchema.pre('save', function(next) { 
+    if (!this.isModified('password')) {
+        return next();
+    }
+    bcrypt.hash(this.password, null, null, (err, hash) => {
+        if (err) return next(err);
+        this.password = hash;
+        next();
+    })
+});
+
+userSchema.methods.comparePassword = (password) => {
+    return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
